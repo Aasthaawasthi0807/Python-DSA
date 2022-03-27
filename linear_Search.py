35a5eec5d68d3a7a8f039e843848a9f683c50d3e
Aastha <awasthiaastha1999@gmail.com>
2022-03-27 21:08:10 +0530

    Linear search

---
+++ linear_Search.py
@@ -0,0 +1,16 @@
+bool found(arr,n,key)
+
+
+
+
+if __name__ == '__main__':
+    arr = [ 2 , 5 , -9 , 3, 10 , 12 , 24 , 54 , 63]
+    print("Enter the element to search for: ")
+    key = input()
+
+    bool found(arr , 9, key)
+    if(found):
+        print("key is present")
+    else:
+        print("key is absent")
+
