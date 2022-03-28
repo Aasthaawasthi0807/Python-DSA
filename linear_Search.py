ac050f394fbb72f17e4e401183cd292848c77d35
Aastha <awasthiaastha1999@gmail.com>
2022-03-28 21:57:54 +0530

    linear search

--- linear_Search.py
+++ linear_Search.py
@@ -1,16 +1,15 @@
-bool found(arr,n,key)
+def search(arr,n,key):
+    for i in range(0,n):
+        if (arr[i] == key):
+            return i
+    return -1
 
+arr = [2, 3, 4, 10, 40]
+key = int(input("enter the value:",))
+n = len(arr)
 
-
-
-if __name__ == '__main__':
-    arr = [ 2 , 5 , -9 , 3, 10 , 12 , 24 , 54 , 63]
-    print("Enter the element to search for: ")
-    key = input()
-
-    bool found(arr , 9, key)
-    if(found):
-        print("key is present")
-    else:
-        print("key is absent")
-
+found = search(arr,n,key)
+if(found== -1):
+    print("key is absent")
+else:
+    print("key is present")
